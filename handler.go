efaac8347f57648e10a9d07bd5ba8befcb10942d
Guillermo Di Pasquale <guilledipasquale@gmail.com>
2019-05-29 15:59:28 +1000

    Implement mapHandler

--- handler.go
+++ handler.go
@@ -17,8 +17,8 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 			http.Redirect(w, r, dest, http.StatusFound)
 			return
 		}
-		return fallback
-
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/guilledipa/urlshort"
+	"proyectos/urlshort"
 )
 
 func main() {
